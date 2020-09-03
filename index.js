9195cd7bd2bc9710a591766e254e024ecaf929e5
xaviguasch <xaviguasch@gmail.com>
2020-09-03 18:18:42 +0200

    solved third challenge

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-// Grab the emojiContainer div and store it in a variable. Log it to the console
+// Render the emojis as <span> tags in the emojisContainer <div>
+
+// Hints: you can achieve this by creating an span with createElement(),
+// setting its content with textContent, and using append() to append it to the container
 
 const myEmojis = ['👨‍💻', '⛷', '🍲']
 const emojiContainer = document.getElementById('emojiContainer')
 
 for (let i = 0; i < myEmojis.length; i++) {
-  // console.log(myEmojis[i])
+  // write your code here
+  const emoji = document.createElement('span')
+  emoji.textContent = myEmojis[i]
+  emojiContainer.append(emoji)
 }
-
-console.log(emojiContainer)
